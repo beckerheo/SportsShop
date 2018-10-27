bf95b58891e3783f032ff5c92c4ba2a9b4c0e8cb
JIA HE <j.he@gothammgt.ca>
2018-10-27 14:24:01 -0400

    1 [CHG] code changed on Oct 27 2018

--- SportsShop.WebApp/Global.asax.cs
+++ SportsShop.WebApp/Global.asax.cs
@@ -1,13 +1,8 @@
 ﻿namespace SportsShop.WebApp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
-    using Autofac;
-    using Autofac.Integration.Mvc;
     using SportsShop.Domain.Entities;
     using SportsShop.WebApp.Infrastructure.Binders;
 
