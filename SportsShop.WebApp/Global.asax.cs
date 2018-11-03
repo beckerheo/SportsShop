8f0d53b856f1f97478a80275a1c96e1f3bf4101d
JIA HE <j.he@gothammgt.ca>
2018-11-03 14:51:22 -0400

    1 [CHG] Add Custom Data View Engine;

--- SportsShop.WebApp/Global.asax.cs
+++ SportsShop.WebApp/Global.asax.cs
@@ -4,6 +4,7 @@
     using System.Web.Mvc;
     using System.Web.Routing;
     using SportsShop.Domain.Entities;
+    using SportsShop.WebApp.Infrastructure;
     using SportsShop.WebApp.Infrastructure.Binders;
 
     public class MvcApplication : HttpApplication
@@ -13,6 +14,9 @@
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             AutofacConfig.Register();
+
+            //ViewEngines.Engines.Clear();
+            //ViewEngines.Engines.Add(new DebugDataViewEngine());
 
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
